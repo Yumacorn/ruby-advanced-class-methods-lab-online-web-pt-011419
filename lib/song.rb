d09694112bf0154bdb04c775e88452772ed96978
Ryan Mui <reachryanmui@gmail.com>
2019-02-21 23:35:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,16 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    @@all << song
+    song
+  end
 end
