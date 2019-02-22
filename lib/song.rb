7c3d0909909ee8984e9a9679f700b289a82874ec
Ryan Mui <reachryanmui@gmail.com>
2019-02-22 01:06:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,4 +33,7 @@ class Song
   def self.find_by_name(name)
     self.all.find {|s| s.name == name}
   end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name || self.create_by_name(name)
 end
