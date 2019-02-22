7b3c2eb93cd2f58c2601e8c77b96b4a0d2767685
Ryan Mui <reachryanmui@gmail.com>
2019-02-22 02:04:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,8 +48,8 @@ class Song
     newfilename = filename.split(" - ")
 
     song.artist_name = newfilename[0]
-    song.name = "Thundercast"
-    # song.name = newfilename[1].split!(".mp3")
+    # song.name = "Thundercast"
+    song.name = newfilename[1].split!(".mp3")
     song
   end
 end
